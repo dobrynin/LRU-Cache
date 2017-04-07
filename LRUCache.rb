2bc5430631b6e11b451c6999605fe60cab947544
David Dobrynin <David.Dobrynin@gmail.com>
2017-04-07 09:25:18 -0700

    Implements simple LRUCache with poor time efficiency

---
+++ LRUCache.rb
@@ -0,0 +1,40 @@
+class LRUCache
+  def initialize(max_size)
+    @cache = []
+    @max_size = max_size
+  end
+
+  def count
+    @cache.size
+  end
+
+  def add(el)
+    @cache.delete(el) if @cache.include?(el)
+    @cache.unshift(el)
+    @cache.pop if @cache.size > @max_size
+  end
+
+  def show
+    p @cache.reverse
+  end
+end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+johnny_cache.count # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
