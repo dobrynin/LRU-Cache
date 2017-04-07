c8c688f1e48a695f8acd46315374934c5b6efe5d
App Academy Student <student@appacademy.io>
2017-04-07 12:03:35 -0700

    Creates hashing function

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,18 +4,26 @@ end
 
 class Array
   def hash
+    acc = 0
+    each_with_index do |el, i|
+      acc += el ^ i
+    end
+
+    acc
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
+
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    (keys.map(&:to_s).map(&:hash).sort + values.map(&:hash).sort).hash
   end
 end
